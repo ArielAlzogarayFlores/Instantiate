a16a61f9ac4256ab0d36d00fc324f22216df926e
ArielAlzogarayFlores <102029851+ArielAlzogarayFlores@users.noreply.github.com>
2022-05-04 15:56:02 -0300

    finished

---
+++ Assets/CloneObjects.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneObjects : MonoBehaviour
+{
+    int i = 1;
+    public GameObject objectToClone;
+
+    public void InstantiateObject()
+    {
+        while (i < 4)
+        {
+            Instantiate(objectToClone);
+            i++;
+        }
+
+        i = 1;
+    }
+}
